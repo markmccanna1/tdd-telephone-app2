5dc6c77a4d11a5a6cdce8407cf3f19e04879b45b
Apprentice <apprentice@dbc-chicago-004.local>
2013-08-27 13:46:41 -0500

    added topping initialization test

--- tests.rb
+++ tests.rb
@@ -6,19 +6,37 @@ describe Pizza do
   let(:pizza) { Pizza.new("Mark's pizza", "sausage")}
 
   describe '#initialize' do
-    it 'should have a name' do
-      expect(pizza.name).to eql "Mark's pizza"
-    end
+    context "after initialization" do
+      it 'should have a name' do
+        expect(pizza.name).to eql "Mark's pizza"
+      end
 
-    it 'should have a description' do
-      expect(pizza.description).to eql "sausage"
-    end
+      it 'should have a description' do
+        expect(pizza.description).to eql "sausage"
+      end
 
-    it 'should have a bake time' do
-      expect(pizza.bake_time).to eql 0
+      it 'should have a time baked' do
+        expect(pizza.time_baked).to eql 0
+      end
     end
   end
+end
 
-  #make a test that tests bake time = 0, for the third param
 
+describe Topping do
+  let(:topping) {Topping.new("sausage", 10)}
+
+  describe '#initialize' do
+    it 'should have a name' do
+      expect(topping.name).to eql "sausage"
+    end
+
+    it 'should have a required bake time' do
+      expect(topping.required_bake_time).to eql 10
+    end
+
+    it 'should have a time baked of 0' do
+      expect(topping.time_baked).to eql 0
+    end
+  end
 end
